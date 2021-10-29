783edfc783efecd227a0bcbba467b4203ec4ca82
Lukman Hakim <hlukman156@gmail.com>
2021-10-29 15:02:18 +0700

    fix filtering on orm

--- homepage/views.py
+++ homepage/views.py
@@ -6,7 +6,7 @@ from .models import Homepage
 
 def index(request):
     # pylint: disable=maybe-no-member
-    post = Post.objects.all()[:3]
+    post = Post.objects.filter(verified=True)[:3]
     homepage = Homepage.objects.all().first()
     categories = Category.objects.all()
 
@@ -16,7 +16,18 @@ def get_post_by_category(request, category):
 
     # pylint: disable=maybe-no-member
     if category == 'all' or category == 'Semua':
-        post = list(Post.objects.filter(verified=True)[:3].values('title', 'author', 'image', 'ig_account', 'category__category', 'slug'))
+        post = Post.objects.filter(verified=True)[:3].values('title', 'author', 'image', 'ig_account', 'category__category', 'slug')
+
+        data = []
+        data_before = ""
+        for p in post:
+
+            if not p.get("title") in data_before:
+                data.append(p)  
+                data_before = p.get("title")
+
+        post = data
+
     elif category:
         cate = get_object_or_404(Category, slug=category)
         post = list(Post.objects.filter(category=cate, verified=True)[:3].values('title', 'author', 'image', 'ig_account', 'category__category', 'slug'))
