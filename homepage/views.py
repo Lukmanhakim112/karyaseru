0f13f4524a7589a0356440480a5682ba94a23afb
Lukman Hakim <hlukman156@gmail.com>
2021-11-02 11:10:37 +0700

    fixing image posititon, filtering, and yt video

--- homepage/views.py
+++ homepage/views.py
@@ -16,21 +16,25 @@ def get_post_by_category(request, category):
 
     # pylint: disable=maybe-no-member
     if category == 'all' or category == 'Semua':
-        post = Post.objects.filter(verified=True)[:3].values('title', 'author', 'image', 'ig_account', 'category__category', 'slug')
+        post = Post.objects.filter(verified=True).values('title', 'author', 'image','category__category' ,'ig_account', 'slug').distinct()
 
         # need refectoring of this section
         data = []
         data_before = ""
-        for p in post:
+        counter = 0
 
+        for p in post:
             if not p.get("title") in data_before:
                 data.append(p)  
                 data_before = p.get("title")
+            if counter == 3:
+                break;
 
         post = data
 
     elif category:
         cate = get_object_or_404(Category, slug=category)
-        post = list(Post.objects.filter(category=cate, verified=True)[:3].values('title', 'author', 'image', 'ig_account', 'category__category', 'slug'))
+        post = list(Post.objects.filter(category=cate, verified=True)[:3]
+                .values('title', 'author', 'image', 'ig_account', 'category__category', 'slug'))
 
     return JsonResponse({'post': post})
